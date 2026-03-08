df4d9bc0d00a3fc309dc68bdc81254e32816c298
Lukas <134181853+bo2themax@users.noreply.github.com>
2026-03-08 15:22:37 +0100

    macos: fix quick terminal glassy background

--- macos/Sources/Features/QuickTerminal/QuickTerminalController.swift
+++ macos/Sources/Features/QuickTerminal/QuickTerminalController.swift
@@ -625,6 +625,8 @@ class QuickTerminalController: BaseTerminalController {
             window.isOpaque = true
             window.backgroundColor = .windowBackgroundColor
         }
+
+        terminalViewContainer?.ghosttyConfigDidChange(ghostty.config, preferredBackgroundColor: nil)
     }
 
     private func showNoNewTabAlert() {

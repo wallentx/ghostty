ea262cdd34c36ac848ddd417cdf29a4dc93d7fb6
Lukas <134181853+bo2themax@users.noreply.github.com>
2026-03-12 18:25:20 +0100

    macOS: fix window cascading for 3rd+ window

--- macos/Sources/Features/Terminal/TerminalController.swift
+++ macos/Sources/Features/Terminal/TerminalController.swift
@@ -256,6 +256,8 @@ class TerminalController: BaseTerminalController, TabGroupCloseCoordinator.Contr
         // take effect. Our best theory is there is some next-event-loop-tick logic
         // that Cocoa is doing that we need to be after.
         DispatchQueue.main.async {
+            c.showWindow(self)
+
             // Only cascade if we aren't fullscreen.
             if let window = c.window {
                 if !window.styleMask.contains(.fullScreen) {
@@ -263,8 +265,6 @@ class TerminalController: BaseTerminalController, TabGroupCloseCoordinator.Contr
                     Self.applyCascade(to: window, hasFixedPos: hasFixedPos)
                 }
             }
-
-            c.showWindow(self)
 
             // All new_window actions force our app to be active, so that the new
             // window is focused and visible.

332b2aefc6e72d363aa93ab6ecfc86eeeeb5ed28
Mitchell Hashimoto <m@mitchellh.com>
2026-03-13 08:56:24 -0700

    1.3.1

--- nix/package.nix
+++ nix/package.nix
@@ -30,7 +30,7 @@
 in
   stdenv.mkDerivation (finalAttrs: {
     pname = "ghostty";
-    version = "1.3.0";
+    version = "1.3.1";
 
     # We limit source like this to try and reduce the amount of rebuilds as possible
     # thus we only provide the source that is needed for the build
